08ed1ae055272898d98a9da7e5d676f5289ae214
Nath <nathaniel@aims.ac.za>
2022-01-20 18:44:59 +0000

    Created customer service as submodule

---
+++ microservice-app/order/src/test/java/com/minejava/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minejava.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
